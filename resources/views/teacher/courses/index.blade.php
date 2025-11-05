610179ff6c4aae8d5c06ee8adbb89945dfa8ed97
Tehuel <torresbaldi.tehuel@gmail.com>
2025-11-04 21:13:19 -0300

    use actions dropdown

--- resources/views/teacher/courses/index.blade.php
+++ resources/views/teacher/courses/index.blade.php
@@ -20,43 +20,63 @@
         @foreach ($courses as $course)
             <div class="col-12 col-sm-6">
                 <div class="card h-100">
-                    <div class="card-body position-relative">
-                        <h1 class="m-0 h2">
-                            <a
-                                href="{{ route('teacher.courses.show', $course) }}"
-                                class="stretched-link text-decoration-none"
-                            >
-                                {{ $course->title }}
-                            </a>
-                        </h1>
-                        <span class="text-muted">{{ $course->description }}</span>
-                    </div>
-                    <div class="card-footer text-muted">
-                        <form method="POST" action="{{ route('teacher.courses.destroy', $course) }}">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-                                <a
-                                    href="{{ route('teacher.courses.show', $course) }}"
-                                    class="btn btn-link"
-                                >
-                                    <i class="bi bi-eye"></i>
-                                    <span class="d-none d-sm-inline">{{ __('Ver') }}</span>
-                                </a>
-                                <a
-                                    href="{{ route('teacher.courses.edit', $course) }}"
-                                    class="btn btn-link"
-                                >
-                                    <i class="bi bi-pencil"></i>
-                                    <span class="d-none d-sm-inline">{{ __('Editar') }}</span>
-                                </a>
+                    <div class="card-body">
+                        <div class="row">
+                            <!-- Course title -->
+                            <div class="col">
+                                <h1 class="m-0 h4">
+                                    <a
+                                        href="{{ route('teacher.courses.show', $course) }}"
+                                        class="text-decoration-none"
+                                    >
+                                        {{ $course->title }}
+                                    </a>
+                                </h1>
+
+                            </div>
+
+                            <!-- Actions dropdown -->
+                            <div class="col-auto">
                                 <button
-                                    type="submit"
-                                    class="btn btn-link text-danger"
+                                    class="btn btn-sm rounded-circle"
+                                    type="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
                                 >
-                                    <i class="bi bi-x-circle"></i>
-                                    <span class="d-none d-sm-inline">{{ __('Eliminar') }}</span>
+                                    <i class="bi bi-three-dots-vertical"></i>
                                 </button>
-                        </form>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li>
+                                        <a
+                                            href="{{ route('teacher.courses.edit', $course) }}"
+                                            class="dropdown-item"
+                                        >
+                                            <i class="bi bi-pencil"></i>
+                                            {{ __('Editar') }}
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <form method="POST" action="{{ route('teacher.courses.destroy', $course) }}">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+                                            <button
+                                                type="submit"
+                                                class="dropdown-item text-danger"
+                                                onclick="return confirm('{{ __('¿Estás seguro de eliminar este curso?') }}')"
+                                            >
+                                                <i class="bi bi-trash"></i>
+                                                {{ __('Eliminar') }}
+                                            </button>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <!-- Course description -->
+                            <div class="col-12">
+                                <span class="text-muted">{{ $course->description ?? __('Sin descripción') }}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
