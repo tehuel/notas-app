5f23ad8d4bdbd4ae878a373b95b319cfbc2f65e7
Tehuel <torresbaldi.tehuel@gmail.com>
2025-11-08 13:07:37 -0300

    adds footer to guest template

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -13,7 +13,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" rel="stylesheet">
     </head>
-    <body>
+    <body class="d-flex flex-column min-vh-100">
         <x-guest-navbar />
 
         <div class="container">
@@ -37,7 +37,25 @@
 
             {{ $slot }}
         </div>
-        
+
+        <div class="container mt-auto">
+            <footer class="py-3 my-4 border-top pt-4 text-center text-muted">
+                <p class="text-muted">
+                    <span>{{ now()->format('Y') }} {{ config('app.name') }}</span>
+                    <span class="mx-2">&middot;</span>
+                    <span>{{ round(microtime(true) - LARAVEL_START, 2) }}s</span>
+                </p>
+                <p class="text-muted">
+                    <a href="https://github.com/tehuel/notas-app" target="_blank">
+                        {{ __('Ver Código Fuente') }}
+                    </a>
+                    <span class="mx-2">&middot;</span>
+                    <a href="https://github.com/tehuel/notas-app/issues" target="_blank">
+                        {{ __('Reportar Error') }}
+                    </a>
+                </p>
+            </footer>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
